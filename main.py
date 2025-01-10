9a852469fb474fd625cc2feac105c25e8d05c821
hunter-tx <hughesh97@outlook.com>
2025-01-09 20:53:48 -0600

    Set max FPS to 60

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     game_over = False
     while not game_over:
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
         # print(f"Screen width: {SCREEN_WIDTH}")
         # print(f"Screen height: {SCREEN_HEIGHT}")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
